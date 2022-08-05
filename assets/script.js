bfa31dd05d580f00cd265ab827babf94a4e0f743
WindaMarito1145 <17191145@bsi.ac.id>
2022-08-05 13:56:33 +0700

    memperbaiki kesalahan pada code

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,32 @@
-//objek 
 const calculator = {
     displayNumber: '0',
     operator: null,
     firstNumber: null,
     isWaitForSecondNumber: false,
-};
-
-//fungsi umum kalkulator
-function updateDisplay() {
-    document.querySelector('#displayNumber').innerText = calculator.displayNumber;   
-}
-
-function clearCalculator() {
+  };
+   
+  function updateDisplay() {
+    document.querySelector('#displayNumber').innerText = calculator.displayNumber;
+  }
+   
+  function clearCalculator() {
     calculator.displayNumber = '0';
     calculator.operator = null;
     calculator.firstNumber = null;
     calculator.isWaitForSecondNumber = false;
-}
-
-//fungsi memasukan angka
-function inputDigit(digit) {
+  }
+   
+  function inputDigit(digit) {
     calculator.displayNumber += digit;
-}
-
-//variabel button
-const buttons = document.querySelectorAll(".button");
-
-for (const button of buttons) {
-   button.addEventListener('click', function (event){
-    
-    //mendapatkan objek elemen yang di klik
-    const target = Event.target;
-    
-    inputDigit(target.innerText);
-    updateDisplay();
-   });
-    
-}
+  }
+   
+  const buttons = document.querySelectorAll('.button');
+  for (const button of buttons) {
+    button.addEventListener('click', function (event) {
+      // mendapatkan objek elemen yang diklik
+      const target = event.target;
+      
+      inputDigit(target.innerText);
+      updateDisplay();
+    });
+  }
