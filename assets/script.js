ea59014547f63c154f41f6f6e309f0dd5597adae
WindaMarito1145 <17191145@bsi.ac.id>
2022-08-05 13:58:16 +0700

    Memodifikasi code pada JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,39 @@
+//objek 
 const calculator = {
     displayNumber: '0',
     operator: null,
     firstNumber: null,
     isWaitForSecondNumber: false,
-  };
-   
-  function updateDisplay() {
-    document.querySelector('#displayNumber').innerText = calculator.displayNumber;
-  }
-   
-  function clearCalculator() {
+};
+
+//fungsi umum kalkulator
+function updateDisplay() {
+    document.querySelector('#displayNumber').innerText = calculator.displayNumber;   
+}
+
+function clearCalculator() {
     calculator.displayNumber = '0';
     calculator.operator = null;
     calculator.firstNumber = null;
     calculator.isWaitForSecondNumber = false;
-  }
-   
-  function inputDigit(digit) {
+}
+
+//fungsi memasukan angka
+function inputDigit(digit) {
     calculator.displayNumber += digit;
-  }
-   
-  const buttons = document.querySelectorAll('.button');
-  for (const button of buttons) {
-    button.addEventListener('click', function (event) {
-      // mendapatkan objek elemen yang diklik
-      const target = event.target;
-      
-      inputDigit(target.innerText);
-      updateDisplay();
-    });
-  }
+}
+
+//variabel button
+const buttons = document.querySelectorAll(".button");
+
+for (const button of buttons) {
+   button.addEventListener('click', function (event){
+    
+    //mendapatkan objek elemen yang di klik
+    const target = event.target;
+    
+    inputDigit(target.innerText);
+    updateDisplay();
+   });
+    
+}
