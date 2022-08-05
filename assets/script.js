e464412d7bc2c14c7489b4955e37d31b2663ee56
WindaMarito1145 <17191145@bsi.ac.id>
2022-08-05 13:51:28 +0700

    Menambahkan function, dan obejek

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,39 @@
-console.log("Selamat Anda berhasil menggunakan JavaScript");
+//objek 
+const calculator = {
+    displayNumber: '0',
+    operator: null,
+    firstNumber: null,
+    isWaitForSecondNumber: false,
+};
+
+//fungsi umum kalkulator
+function updateDisplay() {
+    document.querySelector('#displayNumber').innerText = calculator.displayNumber;   
+}
+
+function clearCalculator() {
+    calculator.displayNumber = '0';
+    calculator.operator = null;
+    calculator.firstNumber = null;
+    calculator.isWaitForSecondNumber = false;
+}
+
+//fungsi memasukan angka
+function inputDigit(digit) {
+    calculator.displayNumber += digit;
+}
+
+//variabel button
+const buttons = document.querySelectorAll(".button");
+
+for (const button of buttons) {
+   button.addEventListener('click', function (event){
+    
+    //mendapatkan objek elemen yang di klik
+    const target = Event.target;
+    
+    inputDigit(target.innerText);
+    updateDisplay();
+   });
+    
+}
